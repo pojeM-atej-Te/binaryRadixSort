936d8ed74c9da0a74c8c9ca1f67cf3f616cb6652
Matej <matej.poje@student.um.si>
2025-03-25 18:02:39 +0100

    Dodana RadixSort Funckija

--- radix_sort.cpp
+++ radix_sort.cpp
@@ -15,6 +15,24 @@ void bubbleSort(vector<int>& arr) {
     }
 }
 
+// Bubble sort by k-th bit (for later use in Radix Sort)
+void bubbleSortByBit(vector<unsigned char>& arr, int k) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            // Extract k-th bit for comparison
+            int bit1 = (arr[j] >> k) & 1;
+            int bit2 = (arr[j + 1] >> k) & 1;
+            
+            // Stable sort: swap if bit1 > bit2
+            // If bits are equal, maintain original order
+            if (bit1 > bit2) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
 int main(){
     
     vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
